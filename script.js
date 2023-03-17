6336fe3723d0222c0ccb0b93f966d35a078f2edc
alex bocknek <alx.bock@gmail.com>
2023-03-17 12:03:04 -0700

    Created the first button functions that check if the screen number is 0 and if true, replace them with the number of the button id 1-9.

--- script.js
+++ script.js
@@ -17,3 +17,25 @@ function divide (a,b){
 function operate (a, b, operation){
     return operation(a,b);
 }
+
+const buttons = document.querySelectorAll('button');
+
+buttons.forEach(button => {
+    button.addEventListener('click', event =>{
+        if (document.querySelector('#screen').textContent === '0' &&
+            (event.target.id === '1'||
+            event.target.id === '2' ||
+            event.target.id === '3' ||
+            event.target.id === '4' ||
+            event.target.id === '5' ||
+            event.target.id === '6' ||
+            event.target.id === '7' ||
+            event.target.id === '8' ||
+            event.target.id === '9')
+        ){
+            document.querySelector('#screen').textContent = event.target.id;
+        }
+
+    });
+});
+
