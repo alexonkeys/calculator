3c6adbf53c31614cd4046800f5c66f3228352846
alex bocknek <alx.bock@gmail.com>
2023-03-17 13:07:15 -0700

    Reconfigured the buttons to have the number-btn class for thinning out the javascript.

--- script.js
+++ script.js
@@ -23,15 +23,7 @@ const buttons = document.querySelectorAll('button');
 buttons.forEach(button => {
     button.addEventListener('click', event =>{
         if (document.querySelector('#screen').textContent === '0' &&
-            (event.target.id === '1'||
-            event.target.id === '2' ||
-            event.target.id === '3' ||
-            event.target.id === '4' ||
-            event.target.id === '5' ||
-            event.target.id === '6' ||
-            event.target.id === '7' ||
-            event.target.id === '8' ||
-            event.target.id === '9')
+            (event.target.className === 'number-btn')
         ){
             document.querySelector('#screen').textContent = event.target.id;
         }
