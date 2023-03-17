efb8125b940fc18ed5b8703db5e058c783648653
alex bocknek <alx.bock@gmail.com>
2023-03-17 13:23:54 -0700

    Added a variable that stores the value of the number on the screen after each button press.

--- script.js
+++ script.js
@@ -18,17 +18,22 @@ function operate (a, b, operation){
     return operation(a,b);
 }
 
+let value1 = 0;
+let value2 = 0;
+
 const buttons = document.querySelectorAll('button');
 
 buttons.forEach(button => {
     button.addEventListener('click', event =>{
         if (document.querySelector('#screen').textContent === '0' && event.target.className === 'number-btn'){
             document.querySelector('#screen').textContent = event.target.id;
+            value1 = event.target.id;
         } else if (document.querySelector('#screen').textContent !== '0' &&
         event.target.className === 'number-btn' &&
         document.querySelector('#screen').textContent.length < 11){
             document.querySelector('#screen').textContent = 
             document.querySelector('#screen').textContent + event.target.id;
+            value1 = document.querySelector('#screen').textContent + event.target.id;
         };
     });
 });
