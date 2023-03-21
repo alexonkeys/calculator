b7089f3e5fd60e3c898c705e70c5ca42babe2803
alex bocknek <alx.bock@gmail.com>
2023-03-21 16:57:43 -0700

    Refactored the code to have opClicked = a string that responds to the operation button clicked. Also renamed the operation buttons in the html.

--- script.js
+++ script.js
@@ -20,10 +20,11 @@ function operate (a, b, operation){
 
 let value1 = 0;
 let value2 = 0;
-let opClicked = false;
+let opClicked = null;
 
 const numberBtns = document.querySelectorAll('.number-btn');
 const opBtns = document.querySelectorAll('.op-btn');
+const eqBtn = document.querySelector('#equals');
 
 numberBtns.forEach(button => {
     button.addEventListener('click', event =>{
@@ -34,19 +35,19 @@ numberBtns.forEach(button => {
         } else if (document.querySelector('#screen').textContent !== '0' 
         && event.target.className === 'number-btn' 
         && document.querySelector('#screen').textContent.length < 11 
-        && opClicked === false){
+        && opClicked === null){
             document.querySelector('#screen').textContent = 
             document.querySelector('#screen').textContent + event.target.id;
             value1 = parseInt(document.querySelector('#screen').textContent);
         } else if (document.querySelector('#screen').textContent === value1.toString()  
         && event.target.className === 'number-btn' 
-        && opClicked === true){
+        && opClicked === ('add'||'subtract'||'multiply'||'divide')){
             document.querySelector('#screen').textContent = event.target.id;
             value2 = parseInt(event.target.id);
         } else if (document.querySelector('#screen').textContent !== '0' 
         && event.target.className === 'number-btn' 
         && document.querySelector('#screen').textContent.length < 11 
-        && opClicked === true){
+        && opClicked === ('add'||'subtract'||'multiply'||'divide')){
             document.querySelector('#screen').textContent = 
             document.querySelector('#screen').textContent + event.target.id;
             value2 = parseInt(document.querySelector('#screen').textContent);
@@ -56,10 +57,24 @@ numberBtns.forEach(button => {
 
 opBtns.forEach(button =>{
     button.addEventListener('click', event =>{
-        if (value1 !== 0){
-            opClicked = true;
-        }
-    })
+        if (value1 !== 0 && event.target.id === 'add'){
+            opClicked = 'add';
+        } else if (value1 !== 0 && event.target.id === 'subtract'){
+            opClicked = 'subtract';
+        } else if (value1 !== 0 && event.target.id === 'multiply'){
+            opClicked = 'multiply';
+        } else if (value1 !== 0 && event.target.id === 'divide'){
+            opClicked = 'divide';
+        };
+    });
 });
+
+eqBtn.addEventListener('click', event =>{
+    if (opClicked === tru && value1 !==0
+    ){
+        operate (value1, value2,)
+
+    }
+})
 
 
