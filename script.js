c26b29ab0c57c496d0b39b5b50160f5375a41222
alex bocknek <alx.bock@gmail.com>
2023-03-20 17:23:31 -0700

    Started building out the code that excepts opBtn presses. Still assessing an issue that the screen text content isn't equal to the value1 toString content in the if statement

--- script.js
+++ script.js
@@ -20,22 +20,38 @@ function operate (a, b, operation){
 
 let value1 = 0;
 let value2 = 0;
+let opClicked = false;
 
 const numberBtns = document.querySelectorAll('.number-btn');
+const opBtns = document.querySelectorAll('.op-btn');
 
 numberBtns.forEach(button => {
     button.addEventListener('click', event =>{
-        if (document.querySelector('#screen').textContent === '0' && event.target.className === 'number-btn'){
+        if (document.querySelector('#screen').textContent === '0'
+        && event.target.className === 'number-btn'){
             document.querySelector('#screen').textContent = event.target.id;
             value1 = parseInt(event.target.id);
-        } else if (document.querySelector('#screen').textContent !== '0' &&
-        event.target.className === 'number-btn' &&
-        document.querySelector('#screen').textContent.length < 11){
+        } else if (document.querySelector('#screen').textContent !== '0' 
+        && event.target.className === 'number-btn' 
+        && document.querySelector('#screen').textContent.length < 11 
+        && opClicked === false){
             document.querySelector('#screen').textContent = 
             document.querySelector('#screen').textContent + event.target.id;
             value1 = parseInt(document.querySelector('#screen').textContent + event.target.id);
+        } else if (document.querySelector('#screen').textContent === value1.toString()  
+        && event.target.className === 'number-btn' 
+        && opClicked === true){
+            document.querySelector('#screen').textContent = event.target.id;
         };
     });
+});
+
+opBtns.forEach(button =>{
+    button.addEventListener('click', event =>{
+        if (value1 !== 0){
+            opClicked = true;
+        }
+    })
 });
 
 
