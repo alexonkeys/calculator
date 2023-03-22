2766434aa9d1aa11cfdbc274b098f7d10c2d4887
alex bocknek <alx.bock@gmail.com>
2023-03-21 17:21:17 -0700

    Configured the equals button to actually move calculations along.

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ function operate (a, b, operation){
 let value1 = 0;
 let value2 = 0;
 let opClicked = null;
+let result = 0;
 
 const numberBtns = document.querySelectorAll('.number-btn');
 const opBtns = document.querySelectorAll('.op-btn');
@@ -41,13 +42,13 @@ numberBtns.forEach(button => {
         value1 = parseInt(document.querySelector('#screen').textContent);
       } else if (document.querySelector('#screen').textContent === value1.toString()
         && event.target.className === 'number-btn'
-        && (opClicked === 'add' || opClicked === 'subtract' || opClicked === 'multiply' || opClicked === 'divide')) {
+        && (opClicked === add || opClicked === subtract || opClicked === multiply || opClicked === divide)) {
         document.querySelector('#screen').textContent = event.target.id;
         value2 = parseInt(event.target.id);
       } else if (document.querySelector('#screen').textContent !== '0'
         && event.target.className === 'number-btn'
         && document.querySelector('#screen').textContent.length < 11
-        && (opClicked === 'add' || opClicked === 'subtract' || opClicked === 'multiply' || opClicked === 'divide')) {
+        && (opClicked === add || opClicked === subtract || opClicked === multiply || opClicked === divide)) {
         document.querySelector('#screen').textContent =
           document.querySelector('#screen').textContent + event.target.id;
         value2 = parseInt(document.querySelector('#screen').textContent);
@@ -58,23 +59,24 @@ numberBtns.forEach(button => {
 opBtns.forEach(button =>{
     button.addEventListener('click', event =>{
         if (value1 !== 0 && event.target.id === 'add'){
-            opClicked = 'add';
+            opClicked = add;
         } else if (value1 !== 0 && event.target.id === 'subtract'){
-            opClicked = 'subtract';
+            opClicked = subtract;
         } else if (value1 !== 0 && event.target.id === 'multiply'){
-            opClicked = 'multiply';
+            opClicked = multiply;
         } else if (value1 !== 0 && event.target.id === 'divide'){
-            opClicked = 'divide';
+            opClicked = divide;
         };
     });
 });
 
 eqBtn.addEventListener('click', event =>{
-    if (opClicked === true && value1 !==0
-    ){
-        operate (value1, value2,)
-
-    }
-})
+    if (opClicked && value1 !==0
+    && result < 99999999999){
+        result = operate(value1,value2,opClicked);
+        document.querySelector('#screen').textContent = result.toString();
+        value1 = result;
+    };
+});
 
 
